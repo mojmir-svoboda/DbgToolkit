b0adc72f0d39f08827f86fc54bf9f0fa4f594f00
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2012-01-16 01:41:05 +0100

    + editable settings for columns +auto-scroll

---
+++ trace_server/settings.cpp
@@ -0,0 +1,107 @@
+#include "settings.h"
+#include <QLineEdit>
+
+SettingsModelView::SettingsModelView (QList<QString> const & app_names, QList<MainWindow::columns_setup_t> const & col_setup)
+	: QAbstractTableModel(0)
+	, m_app_names(app_names)
+	, m_rows()
+{
+	for (size_t i = 0, ie = app_names.size(); i < ie; ++i)
+	{
+		m_rows.push_back(QString());
+
+		for (size_t j = 0, je = col_setup.at(i).size(); j < je; ++j)
+			m_rows.back() += col_setup.at(i).at(j) + QString(", ");
+	}
+}
+
+SettingsModelView::~SettingsModelView () { }
+
+int SettingsModelView::rowCount (const QModelIndex & parent) const { return m_app_names.size(); }
+
+int SettingsModelView::columnCount (const QModelIndex & parent) const { return 2; }
+
+QVariant SettingsModelView::data (const QModelIndex & index, int role) const
+{
+	if (role == Qt::DisplayRole)
+	{
+		QString str("");
+		if (index.column() == 0)
+			if (index.row() < m_app_names.size())
+				return m_app_names[index.row()];
+		if (index.column() == 1)
+			if (index.row() < (int)m_rows.size())
+				return m_rows[index.row()];
+		return str;
+	}
+	return QVariant();
+}
+
+QVariant SettingsModelView::headerData (int section, Qt::Orientation orientation, int role) const
+{
+	if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
+	{
+		if (section == 0)
+			return QString("Application name");
+		if (section == 1)	
+			return QString("tags separated by ,");
+	}
+	return QVariant();
+}
+
+Qt::ItemFlags SettingsModelView::flags (QModelIndex const & index) const
+{
+	 return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+}
+
+SettingsEditDelegate::SettingsEditDelegate(QObject *parent)
+        : QItemDelegate(parent) {}
+
+QWidget *SettingsEditDelegate::createEditor(QWidget *parent,
+                                          const QStyleOptionViewItem &,
+                                          const QModelIndex &index) const
+{
+    QLineEdit *editor = new QLineEdit(parent);
+    connect(editor, SIGNAL(editingFinished()), this, SLOT(commitAndCloseEditor()));
+    return editor;
+}
+
+void SettingsEditDelegate::commitAndCloseEditor()
+{
+    QLineEdit *editor = qobject_cast<QLineEdit *>(sender());
+    emit commitData(editor);
+    emit closeEditor(editor);
+}
+
+void SettingsEditDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+    QLineEdit *edit = qobject_cast<QLineEdit*>(editor);
+    if (edit) {
+        edit->setText(index.model()->data(index, Qt::EditRole).toString());
+    } else {
+/*        QDateTimeEdit *dateEditor = qobject_cast<QDateTimeEdit *>(editor);
+        if (dateEditor) {
+            dateEditor->setDate(QDate::fromString(
+            index.model()->data(index, Qt::EditRole).toString(),
+            "d/M/yyyy"));
+        }*/
+    }
+}
+
+void SettingsEditDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    QLineEdit *edit = qobject_cast<QLineEdit *>(editor);
+    if (edit) {
+        model->setData(index, edit->text());
+    } else {
+/*        QDateTimeEdit *dateEditor = qobject_cast<QDateTimeEdit *>(editor);
+        if (dateEditor) {
+            model->setData(index, dateEditor->date().toString("dd/M/yyyy"));
+        }*/
+    }
+}
+
+
+
+
+
---
+++ trace_server/settings.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <QAbstractTableModel>
+#include <QItemDelegate>
+#include <QObject>
+#include "mainwindow.h"
+
+struct SettingsModelView : QAbstractTableModel
+{
+	Q_OBJECT
+public:
+	explicit SettingsModelView (QList<QString> const & app_names, QList<MainWindow::columns_setup_t> const & col_setup);
+	virtual ~SettingsModelView ();
+	int rowCount (const QModelIndex & parent = QModelIndex()) const;
+	int columnCount (const QModelIndex & parent = QModelIndex()) const;
+	QVariant data (const QModelIndex & index, int role = Qt::DisplayRole) const;
+	QVariant headerData (int section, Qt::Orientation orientation, int role) const;
+	Qt::ItemFlags flags (QModelIndex const & index) const;
+
+signals:
+	
+public slots:
+
+public:
+	QList<QString> const & m_app_names;
+	std::vector<QString> m_rows;
+};
+
+class SettingsEditDelegate : public QItemDelegate
+{
+	Q_OBJECT
+
+public:
+	SettingsEditDelegate (QObject * parent = 0);
+	QWidget * createEditor (QWidget * parent, const QStyleOptionViewItem &, const QModelIndex &index) const;
+	void setEditorData (QWidget * editor, const QModelIndex & index) const;
+	void setModelData (QWidget * editor, QAbstractItemModel * model, const QModelIndex &index) const;
+
+private slots:
+	void commitAndCloseEditor ();
+};
+
