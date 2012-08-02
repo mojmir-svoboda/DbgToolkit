08a3b0ae0c5e3add9b5b2f678eefc425e81b3863
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2012-08-02 18:11:34 +0200

    * last minute fixes

--- trace_server/history.h
+++ trace_server/history.h
@@ -4,7 +4,7 @@
 
 struct ItemInfo
 {
-	int m_use_count;
+	unsigned m_use_count;
 
 	ItemInfo () : m_use_count(0) { }
 };
@@ -27,11 +27,24 @@ struct History
 		if (it == m_dict.end())
 		{
 			if (n > m_item_limit)
-			{
 				evict();
-			}
-			it = m_dict.insert(std::make_pair(k, ItemInfo()));
+			it = m_dict.insert(std::make_pair(k, ItemInfo())).second;
 		}
 		++(it->second.m_use_count);
+	}
+
+	void evict ()
+	{
+		dict_t::iterator it = m_dict.begin(), ite = m_dict.end();
+		dict_t::iterator evict_it = m_dict.end();
+		unsigned min = ~(0U);
+		while (it != ite)
+		{
+			if (it->second.m_use_count < min)
+				evict_it = it;
+			++it;
+		}
+		if (evict_it != m_dict.end())
+			m_dict.erase(evict_it);
 	}
 };
