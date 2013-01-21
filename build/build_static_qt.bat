4be35aa05eb2410adfccebb76015da413d1c74c9
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2013-01-21 17:06:58 +0100

    * qt git build

--- build/build_static_qt.bat
+++ build/build_static_qt.bat
@@ -3,4 +3,5 @@ rem QMAKE_LFLAGS_RELEASE = $$QMAKE_LFLAGS_RELEASE_WITH_DEBUGINFO
 rem
 
 set PATH=C:\strawberry\perl\bin;%PATH%
-configure.bat -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake demos -nomake examples -nomake tests
+rem configure.bat -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake demos -nomake examples -nomake tests
+configure -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-libtiff -no-libmng -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-phonon -no-multimedia -no-webkit -nomake demos -nomake examples -nomake tests
