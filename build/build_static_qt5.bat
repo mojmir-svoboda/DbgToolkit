75b07a7d01e448afa340ecd281298482597c891f
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2013-01-31 12:47:53 +0100

    * script

--- build/build_static_qt5.bat
+++ build/build_static_qt5.bat
@@ -7,22 +7,10 @@ set PATH=C:/strawberry/perl/bin;%PATH%
 set PATH=C:\devel\QtSDK\src\qt5\gnuwin32\bin;%PATH%
 git clone git://gitorious.org/qt/qt5.git qt5
 rem cd qt5
-rem perl init-repository --no-webkit
+rem perl init-repository --no-webkit --codereview-user mojmir.svoboda
 rem configure.bat -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake demos -nomake examples -nomake tests
 
 rem profile
-rem configure -force-debug-info -debug-and-release -opensource -platform win32-msvc2012 -static -no-ltcg -no-accessibility -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -nomake demos -nomake examples -nomake tests -no-nis -angle -directwrite -no-qml-debug -no-opengl
-
-rem release
-rem configure -force-debug-info -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-libtiff -no-libmng -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-phonon -no-multimedia -no-webkit -nomake demos -nomake examples -nomake tests
-rem
-rem 
-
-
-rem *  -no-directwrite .... Do not build support for DirectWrite font rendering.
-rem    -directwrite ....... Build support for DirectWrite font rendering 
-rem                         (experimental, requires DirectWrite availability on 
-rem                         target systems, e.g. Windows Vista with Platform 
-rem                         Update, Windows 7, etc.)
+rem configure -force-debug-info -debug-and-release -opensource -platform win32-msvc2012 -static -no-ltcg -no-accessibility -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -nomake demos -nomake examples -nomake tests -no-nis -angle -no-directwrite -no-qml-debug
 
 rem nmake
