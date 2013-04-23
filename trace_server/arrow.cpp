cc6c66c693c6e749ed6e20046937f5b5490b77c0
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2013-04-23 13:49:12 +0200

    + arrows debug

--- trace_server/arrow.cpp
+++ trace_server/arrow.cpp
@@ -14,7 +14,8 @@ Arrow::Arrow (QGraphicsItem * startItem, QGraphicsItem * endItem, QPointF const 
 {
 	setFlag(QGraphicsItem::ItemIsSelectable, false);
 	m_color = Qt::black;
-	setPen(QPen(m_color, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	//setPen(QPen(m_color, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+	//setPen(QPen(m_color, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
 }
 
 QRectF Arrow::boundingRect () const
@@ -44,43 +45,35 @@ void Arrow::paint (QPainter * painter, QStyleOptionGraphicsItem const *, QWidget
 	if (m_startItem->collidesWithItem(m_endItem))
 		return;
 
+	qreal const arrowSize = 4;
 	QPen p = pen();
 	p.setColor(m_color);
-	qreal const arrowSize = 3;
 	p.setWidth(0);
 	painter->setPen(p);
 	painter->setBrush(m_color);
 
-	QRectF const rect = m_startItem->boundingRect();
-
-	QPointF const start_pt = QPointF(m_endItem->pos().x(), m_startItem->pos().y() + rect.height());
-	QPointF const end_pt = QPointF(m_endItem->pos().x(), m_endItem->pos().y());
-	QLineF const centerLine(start_pt, end_pt);
-
-	QPolygonF const endPolygon = m_endItem->boundingRect();
-	QPointF p1 = m_pt0;
-	QPointF p2 = m_pt1;
-	QLineF polyLine;
+	QPointF const & p1 = m_pt0;
+	QPointF const & p2 = m_pt1;
 	setLine(QLineF(p2, p1));
 
 	double angle = ::acos(line().dx() / line().length());
 	if (line().dy() >= 0)
 		angle = (Pi * 2) - angle;
 
-	QPointF arrowP1 = line().p1() + QPointF(sin(angle + Pi / 3) * arrowSize, cos(angle + Pi / 3) * arrowSize);
-	QPointF arrowP2 = line().p1() + QPointF(sin(angle + Pi - Pi / 3) * arrowSize, cos(angle + Pi - Pi / 3) * arrowSize);
-
+	QPointF const arrowP1 = line().p1() + QPointF(sin(angle + Pi / 3) * arrowSize, cos(angle + Pi / 3) * arrowSize);
+	QPointF const arrowP2 = line().p1() + QPointF(sin(angle + Pi - Pi / 3) * arrowSize, cos(angle + Pi - Pi / 3) * arrowSize);
 	m_arrowHead.clear();
 	m_arrowHead << line().p1() << arrowP1 << arrowP2;
 	painter->drawLine(line());
 	painter->drawPolygon(m_arrowHead);
+
 	if (isSelected())
 	{
 		painter->setPen(QPen(m_color, 1, Qt::DashLine));
-		QLineF myLine = line();
-		myLine.translate(0, 4.0);
-		painter->drawLine(myLine);
-		myLine.translate(0,-8.0);
-		painter->drawLine(myLine);
+		QLineF l = line();
+		l.translate(0, 4.0);
+		painter->drawLine(l);
+		l.translate(0,-8.0);
+		painter->drawLine(l);
 	}
 }
