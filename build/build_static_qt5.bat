9ca824c68e2a34d3d4fbf788beebd48220bc32bc
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2013-12-31 13:45:48 +0100

    * modified paths

--- build/build_static_qt5.bat
+++ build/build_static_qt5.bat
@@ -5,11 +5,22 @@ rem
 set PATH=c:/Program Files (x86)/Git/bin;%PATH%
 set PATH=C:/strawberry/perl/bin;%PATH%
 set PATH=c:\devel\qt5\qtbase\gnuwin32\bin;%PATH%
-set PATH=C:\Python32;%PATH%
+set PATH=C:\Python33;%PATH%
 git clone git://gitorious.org/qt/qt5.git qt5
 cd qt5
 perl init-repository --no-webkit
+
+rem developpers:
 rem perl init-repository --no-webkit --codereview-user mojmir.svoboda
+rem update repo:
+rem
+rem git pull
+rem git submodule sync
+rem git submodule update --recursive
+
+rem clean:
+rem git submodule foreach --recursive "git clean -dfx"
+
 configure.bat -debug-and-release -opensource -platform win32-msvc2012 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake examples -nomake tests -no-icu
 
 rem profile
