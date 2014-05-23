4f3abf677a05a4546df313d9cb432bcfe3c55fa9
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2014-05-23 11:04:16 +0200

    * csv split

--- trace_server/setupdialogcsv.h
+++ trace_server/setupdialogcsv.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <QDialog>
+#include <QComboBox>
 
-namespace Ui {
-class SetupDialogCSV;
-}
+namespace Ui { class SetupDialogCSV; }
+
+enum E_ImportAction {
+	e_Action_Import = 0,
+	e_Action_Skip
+};
 
 class SetupDialogCSV : public QDialog
 {
@@ -24,4 +28,21 @@ public:
 	QStringList m_data;
 	std::vector<int> 	m_column_actions;
 };
+
+inline QString getSeparator (QComboBox * box)
+{
+	if (!box->isEnabled())
+		return QString();
+
+	QString separator;
+	QString const sep_str = box->currentText();
+	if (sep_str == QString("Comma"))          separator = ",";
+	else if (sep_str == QString("Tab"))       separator = "\t";
+	else if (sep_str == QString("Semicolon")) separator = ";";
+	else if (sep_str == QString("Pipe"))      separator = "|";
+	else
+		separator = sep_str;
+	return separator;
+}
+
 
