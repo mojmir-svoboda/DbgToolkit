6e2e0c59f6d2f02c06edda46df0888d6d7104638
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2017-03-29 20:13:34 +0200

    * boost w toolset 141

---
+++ build/boost.bat
@@ -0,0 +1,10 @@
+
+	b2.exe address-model=64 threading=multi toolset=msvc-14.1 link=static
+
+project-config.jam:
+	import option ; 
+	 
+	using msvc : 14.1 : "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\cl.exe"; 
+		
+	option.set keep-going : false ; 
+		 
